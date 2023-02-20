21b80e477aacdffacf44b105fb2c10a5c81bd695
Kira <kira_pavlenko_13@mail.ru>
2023-02-20 22:30:10 +0300

    Initial commit

---
+++ src/test/java/AutoTests.java
@@ -0,0 +1,72 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import usergenerator.UserGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AutoTests {
+
+    UserGenerator generator = new UserGenerator();
+
+        @Test
+        void shouldLoginActive () {
+            generator.authTest("vasya", "create", "active");
+            open("http://localhost:9999/");
+            Configuration.holdBrowserOpen=true;
+            $("[name=login]").setValue("vasya");
+            $("[name=password]").setValue("create");
+            $("button[data-test-id='action-login']").click();
+            $x("//h2[contains(text(),'Личный кабинет')]").shouldHave().should(appear);
+        }
+
+        @Test
+        void shouldNotLoginBlocked () {
+            generator.authTest("vasya", "create", "blocked");
+            open("http://localhost:9999/");
+            Configuration.holdBrowserOpen=true;
+            $("[name=login]").setValue("vasya");
+            $("[name=password]").setValue("create");
+            $("button[data-test-id='action-login']").click();
+            $(".notification__title").shouldHave(Condition.text("Ошибка"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+            $(".notification__content").shouldHave(Condition.text("Ошибка! Пользователь заблокирован"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        }
+
+        @Test
+        void shouldNotLoginUnregistered () {
+            open("http://localhost:9999/");
+            Configuration.holdBrowserOpen=true;
+            $("[name=login]").setValue("petya");
+            $("[name=password]").setValue("create");
+            $("button[data-test-id='action-login']").click();
+            $(".notification__title").shouldHave(Condition.text("Ошибка"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+            $(".notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        }
+
+        @Test
+        void shouldErrorActiveWithWrongPass () {
+            generator.authTest("vasya", "create", "active");
+            open("http://localhost:9999/");
+            Configuration.holdBrowserOpen=true;
+            $("[name=login]").setValue("vasya");
+            $("[name=password]").setValue("creative");
+            $("button[data-test-id='action-login']").click();
+            $(".notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+
+        }
+
+        @Test
+        void shouldErrorBlockedWithWrongPass () {
+            generator.authTest("vasya", "create", "blocked");
+            open("http://localhost:9999/");
+            Configuration.holdBrowserOpen=true;
+            $("[name=login]").setValue("vasya");
+            $("[name=password]").setValue("creative");
+            $("button[data-test-id='action-login']").click();
+            $(".notification__content").shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        }
+ }
+
